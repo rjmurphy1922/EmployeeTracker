c2cdaf2e545db5a481b3bb29fe87d903474ebac8
Ryan <ryan@Ryans-iMac.local>
2021-02-15 11:03:47 -0800

    updated employee prompt functions

---
+++ Develop/app.js
@@ -0,0 +1,93 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const employees = []
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+//name , id, email, officeNumber)
+
+mainMenu = () => {
+
+  managerPrompt = () =>{
+    inquirer
+    .prompt([
+  
+  {
+    type: "input",
+    name:"managerName",
+    message: "What is your name"
+      
+      },
+
+      {
+        type: "input",
+        name:"managerId",
+        message: "What is your employee ID"
+      },
+      {
+        type: "input",
+        name:"managerEmail",
+        message: "What is your email"
+      },
+
+      {
+        type: "input",
+        name:"managerOffice",
+        message: "What is your office"
+      }
+
+    ])
+    .then(answer => {
+    
+      const manager = new Manager(answer.managerName, answer.managerId, answer.managerEmail, answer.managerOffice);
+
+      // managerPrompt()
+
+      employees.push(manager)
+    
+      // render()
+      console.log(employees)
+    
+    })
+  
+    
+    .catch(error => {
+      if (error)
+      throw (error)
+    });
+  
+
+  }
+
+  
+
+}
+
+
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
---
+++ Develop/lib/Intern.js
@@ -0,0 +1,25 @@
+// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Intern = require("./Intern.js");
+
+    class Intern extends Employee{
+
+        constructor(name , id, email, school) {
+        super(name,id,email);
+
+        this.school = school;
+    }
+
+    getSchool(){
+        return this.school
+    }
+
+
+    getRole(){
+        return "Intern"
+    }
+
+}
+
+module.exports = Intern
